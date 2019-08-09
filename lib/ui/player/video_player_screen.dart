aefe512f8153853ccad8a7749662182a42a8316b
Israel Hudson <israelhudson@gmail.com>
2019-08-09 16:17:08 -0300

    instalado o plugin do exoplayer

--- lib/ui/player/video_player_screen.dart
+++ lib/ui/player/video_player_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 import 'package:toast/toast.dart';
+import 'package:media_player/data_sources.dart';
+import 'package:media_player/media_player.dart';
+import 'package:media_player/ui.dart';
 
 class VideoPlayerScreen extends StatefulWidget {
   VideoPlayerScreen({Key key}) : super(key: key);
@@ -10,94 +12,78 @@ class VideoPlayerScreen extends StatefulWidget {
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  VideoPlayerController _controller;
-  Future<void> _initializeVideoPlayerFuture;
+  MediaPlayer player;
+  MediaFile song1 = MediaFile(
+    title: "Song 1",
+    type: "video",
+    //source: "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8",
+    source: "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
+    desc: "Note from Apple",
+  );
 
   @override
   void initState() {
-    // Create and store the VideoPlayerController. The VideoPlayerController
-    // offers several different constructors to play videos from assets, files,
-    // or the internet.
-    _controller = VideoPlayerController.network(
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
-    );
-
-    // Initialize the controller and store the Future for later use.
-    _initializeVideoPlayerFuture = _controller.initialize();
-
-    // Use the controller to loop the video.
-    _controller.setLooping(false);
-
+    // first argument for isBackground next for showNotification.
+    player = MediaPlayerPlugin.create(isBackground: true, showNotification: true);
+    initVideo();
     super.initState();
   }
 
   @override
   void dispose() {
-    // Ensure disposing of the VideoPlayerController to free up resources.
-    alert("FINALIZOU");
-    _controller.dispose();
+    player.dispose();
     super.dispose();
+  }
+
+  void initVideo() async {
+    await player.initialize();
+    await player.setSource(song1);
+    player.play();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-//      appBar: AppBar(
-//        title: Text('Butterfly Video'),
-//      ),
-      // Use a FutureBuilder to display a loading spinner while waiting for the
-      // VideoPlayerController to finish initializing.
-      body: FutureBuilder(
-        future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot) {
-//          if(_controller.value.initialized && !_controller.value.isPlaying){
-//            print("FIM");
-//          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the VideoPlayerController has finished initialization, use
-            // the data it provides to limit the aspect ratio of the video.
-            return AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              // Use the VideoPlayer widget to display the video.
-              child: Container(
-                child: Stack(
-                  children: <Widget>[
-                    VideoPlayer(_controller),
-                    Align(
-                      alignment: Alignment.center,
-                      child: InkWell(
-                          onTap: _playPause,
-                          child: Icon(
-                            _controller.value.isPlaying
-                                ? Icons.pause
-                                : Icons.play_arrow,
-                            size: 80,
-                            color: Colors.white,
-                          )),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else {
-            // If the VideoPlayerController is still initializing, show a
-            // loading spinner.
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+    return Column(children: [
+      VideoPlayerView(player),
+      VideoProgressIndicator(
+        player,
+        allowScrubbing: true,
+        padding: EdgeInsets.symmetric(vertical:5.0),
       ),
-    );
+      SizedBox(height:20.0),
+      buildButtons()
+    ]);
   }
 
-  void _playPause() {
-    setState(() {
-      if (_controller.value.isPlaying) {
-        _controller.pause();
-      } else {
-        // If the video is paused, play it.
-        _controller.play();
-      }
-    });
+  Row buildButtons() {
+    return Row(
+      children: <Widget>[
+        FlatButton(
+          child: Text("Prev"),
+          onPressed: () {
+            player.playPrev();
+          },
+        ),
+        FlatButton(
+          child: Text("Play"),
+          onPressed: () {
+            player.play();
+          },
+        ),
+        FlatButton(
+          child: Text("Pause"),
+          onPressed: () {
+            player.pause();
+          },
+        ),
+        FlatButton(
+          child: Text("Next"),
+          onPressed: () {
+            player.playNext();
+          },
+        ),
+      ],
+    );
   }
 
   void alert(String text){
