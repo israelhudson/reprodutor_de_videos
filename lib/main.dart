c434f3098b9606a19ecb8f41939fbffc4aad6ec2
Israel Hudson <israelhudson@gmail.com>
2019-08-09 08:53:24 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:reprodutor_de_videos/ui/home/home_screen.dart';
+
+void main() => runApp(
+    MaterialApp(
+        home:HomeScreen(),
+        debugShowCheckedModeBanner: false,
+    )
+);
---
+++ lib/ui/player/player_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PlayerScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
