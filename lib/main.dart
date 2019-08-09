0d828acf3dd20e0e2d3b8d0163d885270d3350ed
Israel Hudson <israelhudson@gmail.com>
2019-08-09 09:46:19 -0300

    Player executando

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reprodutor_de_videos/ui/home/home_screen.dart';
+import 'package:reprodutor_de_videos/ui/player/video_player_screen.dart';
 
 void main() => runApp(
     MaterialApp(
--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reprodutor_de_videos/ui/player/video_player_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -13,7 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Player de Videos"),
       ),
       body: Container(
-
+        child: VideoPlayerScreen(),
       ),
     );
   }
